a2d5b44ba242731d86e343f002c476b13216e263
Muhammad Dawood <dawood.duggal2610@gmail.com>
2025-05-17 06:49:00 +0500

    docker commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,12 @@
 import { Routes, Route, useLocation } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 
-import Navbar from "./components/Navbar";
+import Navbar from "./components/navbar";
 import Footer from "./components/footer";
 
 import Home from "./pages/Home";
 import Races from "./pages/Races";
-import Admin from "./pages/admin";
+import Admin from "./pages/Admin";
 import About from "./pages/About";
 import RaceDetails from "./pages/RaceDetails";
 import Login from "./pages/Login";
@@ -16,35 +16,59 @@ import Register from "./pages/Register";
 import "./styles/app.css";
 
 const App = () => {
-  const location = useLocation();
+	const location = useLocation();
 
-  return (
-    <div className="app-layout">
-      <Navbar />
-      <main className="app-main">
-        <AnimatePresence mode="wait">
-          <Routes location={location} key={location.pathname}>
-            <Route path="/" element={<Home />} />
-            <Route path="/races" element={<Races />} />
-            <Route path="/admin" element={<Admin />} />
-            <Route path="/races/:id" element={<RaceDetails />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/register" element={<Register />} />
-            <Route
-              path="/admin"
-              element={
-                <ProtectedRoute>
-                  <Admin />
-                </ProtectedRoute>
-              }
-            />
-          </Routes>
-        </AnimatePresence>
-      </main>
-      <Footer />
-    </div>
-  );
+	return (
+		<div className="app-layout">
+			<Navbar />
+			<main className="app-main">
+				<AnimatePresence mode="wait">
+					<Routes
+						location={location}
+						key={location.pathname}
+					>
+						<Route
+							path="/"
+							element={<Home />}
+						/>
+						<Route
+							path="/races"
+							element={<Races />}
+						/>
+						<Route
+							path="/admin"
+							element={<Admin />}
+						/>
+						<Route
+							path="/races/:id"
+							element={<RaceDetails />}
+						/>
+						<Route
+							path="/login"
+							element={<Login />}
+						/>
+						<Route
+							path="/about"
+							element={<About />}
+						/>
+						<Route
+							path="/register"
+							element={<Register />}
+						/>
+						<Route
+							path="/admin"
+							element={
+								<ProtectedRoute>
+									<Admin />
+								</ProtectedRoute>
+							}
+						/>
+					</Routes>
+				</AnimatePresence>
+			</main>
+			<Footer />
+		</div>
+	);
 };
 
 export default App;
